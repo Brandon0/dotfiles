362da7d8d6d198be6ca92dfb8729c9a07086a4c5
Brandon Kahre <brandon.kahre@peak10.com>
2014-08-27 12:50:07 -0400

    ST2: Sync changes
    Sync changes from Windows work environment over last 3 months

--- Sublime Text 2/Packages/Google Search/google_search.py
+++ Sublime Text 2/Packages/Google Search/google_search.py
@@ -1,27 +1,65 @@
 import webbrowser
 import sublime, sublime_plugin
 
-class GoogleSearchCommand(sublime_plugin.TextCommand):
+def search(q):
+    settings = sublime.load_settings("google_search.sublime-settings")
+    # Attach the suffix and the prefix
+    q = settings.get('prefix', '') + q + settings.get('suffix', '')
 
+    fullUrl = settings.get('domain', 'https://www.google.com') + "/search?q=%s" % q
+
+    webbrowser.open(fullUrl)
+
+
+class GoogleSearchCommand(sublime_plugin.TextCommand):
+    """
+    Search the selected text or the current word
+    """
     def run(self, edit):
 
-        selection = ""
+        querys = []
         for region in self.view.sel():
-            # Concatenate selected regions together.
-            selection += self.view.substr(region)
+            if region.empty():
+                # if we have no selection grab the current word
+                word = self.view.word(region)
+                if not word.empty():
+                    querys.append(self.view.substr(word))
+            else:
+                # append the selection
+                if not region.empty():
+                    querys.append(self.view.substr(region))
 
-        webbrowser.open("http://www.google.com/search?q=%s" % selection)
 
-    def is_visible(self):
+        if len(querys) != 0:
+            selection = " ".join(querys)
+            search(selection)
+        else:
+            sublime.status_message(" Nothing to search !")
+            print("Google Search Plugin: Nothing to search !")
 
-        is_visible = False
 
-        # Only enable menu option if at least one region contains selected text.
-        for region in self.view.sel():
-            if not region.empty():
-                is_visible = True
+class GoogleSearchAnyCommand(sublime_plugin.WindowCommand):
+    """
+    A command that prompts the user to enter search query text.
+    """
 
-        return is_visible
+    def run(self):
 
-    def is_enabled(self):
-        return self.is_visible()
+        def show_input_panel():
+            self.window.show_input_panel(
+                'Enter a search query',
+                '',
+                self.on_done,
+                self.on_change,
+                self.on_cancel
+                )
+        sublime.set_timeout(show_input_panel, 10)
+
+    def on_done(self, text):
+        search(text)
+
+    def on_change(self):
+        pass
+
+    def on_cancel(self):
+        pass
